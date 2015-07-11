ed7dd6566a56d557660b4e5afff52d80283f7df8
보롱킴 <dsdgun@gmail.com>
2015-07-11 23:51:05 +0900

    vector 를 이용하도록 구조 변경

--- js/common.js
+++ js/common.js
@@ -1,1 +1,9 @@
 function rand(min, max) { return min + (Math.random() * (max - min)); }
+function PVector(x,y) {
+	this.x = x;
+	this.y = y;
+}
+PVector.prototype.add = function(v) {
+	this.y = this.y + v.y;
+	this.x = this.x + v.x;
+};
