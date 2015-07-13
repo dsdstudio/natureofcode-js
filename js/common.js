2847e337bf6dc64cc0689b2ab08a866e68ff96e0
보롱킴 <dsdgun@gmail.com>
2015-07-14 01:10:42 +0900

    벡터관련 함수들 대거 추가, 마우스를 따라다니며 가속도 붙는 예제 구현

--- js/common.js
+++ js/common.js
@@ -8,6 +8,34 @@ vec2.prototype.add = function(v) {
 	this.x += v.x;
 };
 vec2.prototype.limit = function(max) {
-	if (this.x > max) this.x = 10;
-	if (this.y > max) this.y = 10;
+	var mSq = this.magSq();
+	if ( mSq > max*max ) {
+		this.div(Math.sqrt(mSq));
+		this.mult(max);
+	}
+	return this;
+};
+vec2.prototype.mult = function(mult) {
+	this.x *= mult;
+	this.y *= mult;
+};
+vec2.prototype.sub = function(v) {
+	this.x -= v.x;
+	this.y -= v.y;
+};
+vec2.prototype.normalize = function(n) {
+	return this.div(this.mag());
+};
+vec2.prototype.div = function(n) {
+	this.x /= n;
+	this.y /= n;
+}
+vec2.prototype.mag = function() { return Math.sqrt(this.magSq()); };
+vec2.prototype.magSq = function() {
+	var x = this.x, y = this.y;
+	return (x*x+y*y);
+};
+Vector = {};
+Vector.sub = function(v1, v2) {
+	return new vec2(v1.x - v2.x, v1.y - v2.y);
 };
